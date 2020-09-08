e329bf9d99ccbea4681d169526f5ddcf90c18533
kristof <kristofdewin1982@gmail.com>
2020-09-08 20:10:33 +0200

    Cleanup GildedRose.java

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,9 +3,6 @@ package com.gildedrose;
 import com.gildedrose.rules.RuleFactory;
 
 class GildedRose {
-    private final int MAX_QUALITY = 50;
-    private final int MIN_QUALITY = 0;
-
     Item[] items;
     RuleFactory factory = new RuleFactory();
 
